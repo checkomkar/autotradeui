569f69c6b3e8f7ceae1902a1b0776e4d12833429
Omkar Kamale <check.omkar@gmail.com>
2024-04-19 01:04:54 +0530

    changes

--- src/app/terminal/components/TradeSection.tsx
+++ src/app/terminal/components/TradeSection.tsx
@@ -11,17 +11,6 @@ import React, { useEffect, useState } from "react";
 
 // import API from "../../../../shoonya/lib/RestApi";
 import Grid from "@mui/material/Unstable_Grid2";
-import { TOTP } from "totp-generator";
-
-const authparams = {
-  userid: "FA26323",
-  password: "Global@99510",
-  twoFA: "OTP/TOTP",
-  vendor_code: "FA26323_U",
-  api_secret: "d4cca4f8be3c72a58a8ee8358ccdd81e",
-  imei: "abc1234",
-  totpToken: "4U35W6I42L53VYXLM7F46XN7AR6Q6PZW",
-};
 
 function TradeSection() {
   // const api = new API({});
